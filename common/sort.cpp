c2987e38ff98e863fd548c0e52ea4f7565d67e4b
Huan <374300671@qq.com>
2021-04-13 11:39:15 +0800

    微修

--- common/sort.cpp
+++ common/sort.cpp
@@ -1,12 +1,5 @@
 #include <stdio.h>
-/*
-@冒泡排序
-比较相邻的元素。如果第一个比第二个大，就交换他们两个。
-对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
-针对所有的元素重复以上的步骤，除了最后一个。
-持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
-https://www.runoob.com/w3cnote/bubble-sort.html
-*/
+
 void swap(int *p, int *q)
 {
     int tmp;
@@ -15,6 +8,14 @@ void swap(int *p, int *q)
     *q=tmp;
     // printf("%d, %d\n", *p, *q);
 }
+/*
+@冒泡排序
+比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+针对所有的元素重复以上的步骤，除了最后一个。
+持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+https://www.runoob.com/w3cnote/bubble-sort.html
+*/
 int bubble_sort(int *arr, int length)
 {
     int len, j;
@@ -37,19 +38,26 @@ int bubble_sort(int *arr, int length)
 }
 /*
 @选择排序
+首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置。
+再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+重复第二步，直到所有元素均排序完毕。
+@注：比较次数O(n^2),交换次数O(n)
 */
 int select_sort(int *arr, int length)
 {
     int i, j;
     for(i=0; i<length-1;++i)
     {
+        int min=i;
         for(j=i+1;j<length; ++j)
         {
-            if(arr[i]>arr[j])
+            if(arr[min]>arr[j])
             {
-                swap(arr+i, arr+j);
+                min=j;
+                
             }
         }
+         swap(arr+i, arr+min);
     }
     return 0;
 }
@@ -70,8 +78,8 @@ int main()
     // 冒泡排序
     // bubble_sort(arr, length);
     //  选择排序
-    // select_sort(arr, length);
-    
+    select_sort(arr, length);
+
     
     print(arr, length);
 }
